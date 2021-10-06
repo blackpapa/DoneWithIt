8e8324474477d6a0cddcf89351a11c7628bf82e8
Andy Chen <andycjx8@gmail.com>
2021-10-06 15:04:14 +1100

    Add screen

--- app/components/MessagesScreen.tsx
+++ app/components/MessagesScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { FlatList } from "react-native";
 import ListItem from "./ListItem";
+import Screen from "./Screen";
 
 interface MessagesScreenProps {}
 
@@ -8,30 +9,32 @@ const messages = [
   {
     id: 1,
     title: "Andy",
-    subTitle: "subtitle",
+    subTitle: "gentlemen",
     image: require("../assets/Andy.jpg"),
   },
   {
     id: 2,
     title: "Rogan",
-    subTitle: "subtitle",
+    subTitle: "gentlemen",
     image: require("../assets/Andy.jpg"),
   },
 ];
 
 const MessagesScreen: React.FC<MessagesScreenProps> = () => {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.subTitle}
-          image={item.image}
-        />
-      )}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.subTitle}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   );
 };
 
