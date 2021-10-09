13dec19de2775f9b7b04746264393774c5ca382f
Andy Chen <andycjx8@gmail.com>
2021-10-09 16:42:59 +1100

    Create and build register Screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import LoginScreen from "./app/components/LoginScreen";
+import RegisterScreen from "./app/components/RegisterScreen";
 
 export default function App() {
-  return <LoginScreen />;
+  return <RegisterScreen />;
 }
---
+++ app/components/RegisterScreen.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { AppForm, AppFormField, SubmitButton } from "./form";
+import * as Yup from "yup";
+
+import Screen from "./Screen";
+import { StyleSheet } from "react-native";
+
+interface RegisterScreenProps {}
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required().label("Name"),
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(5).label("Password"),
+});
+
+const RegisterScreen: React.FC<RegisterScreenProps> = () => {
+  return (
+    <Screen style={styles.container}>
+      <AppForm
+        initialValues={{ name: "", email: "", password: "" }}
+        onSubmit={(value: any) => console.log(value)}
+        validationSchema={validationSchema}
+      >
+        <AppFormField
+          icon="account"
+          name="name"
+          placeholder="Name"
+          autoCorrect={false}
+          autoCapitalize="none"
+          keyboardType="default"
+          textContentType="name"
+          secureTextEntry={true}
+        />
+        <AppFormField
+          icon="email"
+          name="email"
+          placeholder="Username"
+          autoCorrect={false}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          textContentType="emailAddress"
+          secureTextEntry={false}
+        />
+        <AppFormField
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          autoCorrect={false}
+          autoCapitalize="none"
+          keyboardType="default"
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <SubmitButton title="register" />
+      </AppForm>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+});
+
+export default RegisterScreen;
