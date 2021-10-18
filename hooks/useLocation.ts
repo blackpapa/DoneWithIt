db7e95c91f7543d9ebf231f75b931d3e58e4496f
Andy Chen <andycjx8@gmail.com>
2021-10-19 10:43:08 +1100

    Update state variable location

--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -1,8 +1,13 @@
 import  { useEffect, useState } from "react";
 import * as Location from "expo-location";
+
+type Coord = {
+  longitude: number ,
+  latitude: number ,
+}
  
  const useLocation = () => {
-    const [location, setLocation] = useState<Location.LocationObject | null>();
+    const [location, setLocation] = useState<Coord>({longitude: 0, latitude: 0});
 
     const requestLocationPermission = async () => {
       const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -10,7 +15,13 @@ import * as Location from "expo-location";
         alert("You have to enable the permission to use the app");
         return;
       }
-      let location = await Location.getLastKnownPositionAsync();
+      const result = await Location.getLastKnownPositionAsync();
+
+      if(!result ) return null;
+
+      location.latitude = result.coords.latitude
+      location.longitude = result.coords.longitude
+
       setLocation(location);
     };
   
