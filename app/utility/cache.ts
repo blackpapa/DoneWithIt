5baea87a780f1a2461b10b28eba938af62e87bc5
Andy Chen <andycjx8@gmail.com>
2021-10-20 11:06:09 +1100

    Implement cache layer

---
+++ app/utility/cache.ts
@@ -0,0 +1,50 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import moment from "moment";
+
+const prefix = "cache";
+const expiryInMinutes = 5;
+
+//Store item
+const store = async (key: any, value: any) => {
+  try {
+    const item = {
+      value,
+      timestamp: Date.now(),
+    };
+
+    await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Determine if it is expiry
+const isExpired = (item: any) => {
+  const now = moment(Date.now());
+  const storedTime = moment(item.timestamp);
+  return now.diff(storedTime, "minutes") > expiryInMinutes;
+};
+
+//Get item
+const get = async (key: any) => {
+  try {
+    const value = await AsyncStorage.getItem(prefix + key);
+    const item = value? JSON.parse(value) : null;
+
+    if (!item) return null;
+
+    if (isExpired(item)) {
+      AsyncStorage.removeItem(prefix + key);
+      return null;
+    }
+
+    return item.value;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  store,
+  get,
+};
