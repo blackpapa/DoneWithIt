a410c369f88bee37a340c46c47c8c2653515b55c
Andy Chen <andycjx8@gmail.com>
2021-10-22 11:30:01 +1100

    Restore token on mount

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
 import { NavigationContainer } from "@react-navigation/native";
 
 import TabNavigator from "./app/navigation/TabNavigator";
@@ -6,9 +7,22 @@ import navigationTheme from "./app/config/navigationTheme";
 import OfflineNotice from "./app/components/OfflineNotice";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/auth/context";
+import AuthStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState<object>();
+
+  const restoreToken = async () => {
+    const token = await AuthStorage.getToken();
+
+    if (!token) return null;
+
+    setUser(jwtDecode(token));
+  };
+
+  useEffect(() => {
+    restoreToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
