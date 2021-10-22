6622853d6c13fcd91de85e0bd71a67ebbf9fd052
Andy Chen <andycjx8@gmail.com>
2021-10-22 11:30:22 +1100

    Implemnt error handling

--- app/auth/storage.ts
+++ app/auth/storage.ts
@@ -1,15 +1,31 @@
 import * as SecureStore from 'expo-secure-store'
 
-const storageToken = (key: any, value: any) => {
-    return SecureStore.setItemAsync(key, value)
+const key = "authToken"
+
+const storageToken = async (value: any) => {
+    try {
+        
+        return await SecureStore.setItemAsync(key, value)
+    } catch (error) {
+        console.log('Error in storaging the token', error)
+    }
 }
 
-const getToken = (key: any) => {
-    return SecureStore.getItemAsync(key)
+const getToken =  async () => {
+    try {     
+        return await SecureStore.getItemAsync(key)
+    } catch (error) {
+        console.log('Error in getting the token', error)
+    }
 }
 
-const removeToken = (key: any) => {
-    return SecureStore.deleteItemAsync(key)
+const removeToken = async  () => {
+    try {
+        return await SecureStore.deleteItemAsync(key)
+        
+    } catch (error) {
+        console.log('Error in removing the token', error)
+    }
 }
 
 export default {
