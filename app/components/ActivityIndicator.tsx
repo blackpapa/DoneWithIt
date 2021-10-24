4079c6330071ca853127afed9e68d754ec438dfc
Andy Chen <andycjx8@gmail.com>
2021-10-25 09:43:49 +1100

    Let indicator overlay

--- app/components/ActivityIndicator.tsx
+++ app/components/ActivityIndicator.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LottieView from "lottie-react-native";
+import { View, StyleSheet } from "react-native";
 
 interface ActivityIndicatorProps {
   visible: boolean;
@@ -8,12 +9,25 @@ interface ActivityIndicatorProps {
 const ActivityIndicator: React.FC<ActivityIndicatorProps> = ({ visible }) => {
   if (!visible) return null;
   return (
-    <LottieView
-      autoPlay
-      loop
-      source={require("../assets/animations/loader.json")}
-    />
+    <View style={styles.overlay}>
+      <LottieView
+        autoPlay
+        loop
+        source={require("../assets/animations/loader.json")}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    backgroundColor: "white",
+    opacity: 0.8,
+    height: "100%",
+    width: "100%",
+    zIndex: 1,
+  },
+});
 
 export default ActivityIndicator;
